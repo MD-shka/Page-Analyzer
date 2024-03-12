fc1d42c66a2e9858514823a92e8a5ecacbf48073
Mikhail Egorov <pe.egorov@gmail.com>
2024-03-12 10:41:41 +0500

    update db_requests.py

--- page_analyzer/db_requests.py
+++ page_analyzer/db_requests.py
@@ -9,47 +9,46 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+def connect():
+    return psycopg2.connect(DATABASE_URL)
+
+
 def is_unique_url(url):
-    conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+    with connect() as conn:
+        curs = conn.cursor(cursor_factory=NamedTupleCursor)
         curs.execute('SELECT * FROM urls WHERE name=%s;', (url,))
         result = curs.fetchone()
-    conn.close()
     if result:
         return False
     return True
 
 
 def get_url(id):
-    conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+    with connect() as conn:
+        curs = conn.cursor(cursor_factory=NamedTupleCursor)
         curs.execute('SELECT * FROM urls WHERE id=%s;', (id,))
         result = curs.fetchone()
-    conn.close()
     return result
 
 
 def get_urls():
-    conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+    with connect() as conn:
+        curs = conn.cursor(cursor_factory=NamedTupleCursor)
         curs.execute('SELECT * FROM urls ORDER BY  id DESC;')
         urls = curs.fetchall()
-    conn.close()
     return urls
 
 
 def get_url_id(url):
-    conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+    with connect() as conn:
+        curs = conn.cursor(cursor_factory=NamedTupleCursor)
         curs.execute('SELECT * FROM urls WHERE name=%s;', (url,))
         result = curs.fetchone()
-    conn.close()
     return result.id
 
 
 def add_new_url(name_url):
-    conn = psycopg2.connect(DATABASE_URL)
-    with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+    with connect() as conn:
+        curs = conn.cursor(cursor_factory=NamedTupleCursor)
         curs.execute('INSERT INTO urls (name) VALUES (%s);', (name_url,))
-    conn.commit()
-    conn.close()
+        conn.commit()
